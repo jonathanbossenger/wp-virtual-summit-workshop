4388703702030e1a81432fea0ffaea073745f779
Jonathan Bossenger <jonathanbossenger@gmail.com>
2019-08-15 08:33:48 +0200

    Plugin Scaffold, generates starter code for a plugin. https://developer.wordpress.org/cli/commands/scaffold/plugin/

---
+++ wp-virtual-summit-workshop.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     WP Virtual Summit Workshop
+ * Plugin URI:      https://jonathanbossenger.com
+ * Description:     Simple Shortcode based newsletter subscribe form that connects to the MailChimp API to subscribe a user
+ * Author:          Jonathan Bossenger
+ * Author URI:      https://jonathanbossenger.com
+ * Text Domain:     wp-virtual-summit-workshop
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         WP_Virtual_Summit
+ */
+
+// Your code starts here.
