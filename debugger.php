856a15f9820ea4eab9cf8785136c711d6cc2498d
Jonathan Bossenger <jonathanbossenger@gmail.com>
2019-08-16 14:20:20 +0200

    Adding workshop debugger

---
+++ debugger.php
@@ -0,0 +1,13 @@
+<?php
+// For plugins
+function wceu_error_log( $message, $data = '' ) {
+	$file = trailingslashit( plugin_dir_path( __FILE__ ) ) . 'log/' . date( 'Y-m-d' ) . '.log';
+	if ( ! is_file( $file ) ) {
+		file_put_contents( $file, '' );
+	}
+	if ( ! empty( $data ) ) {
+		$message = array( $message => $data );
+	}
+	$data_string = print_r( $message, true ) . "\n";
+	file_put_contents( $file, $data_string, FILE_APPEND );
+}
